345da2270e6e7ca93b5e5049cd120d39eeed7cbf
marcosumali <marcosumali.github.io>
2018-03-15 17:55:58 +0700

    add hasil

--- controllers.js
+++ controllers.js
@@ -2,6 +2,37 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+
+  
+  let testvar = setInterval(function(){
+    
+    let menit = Math.floor(seconds/60);
+    let detik = seconds - (menit*60);
+
+    if (menit < 10 && detik < 10) {
+      // console.log(`0${menit}:0${detik}`);
+      displayCountdown(`0${menit}:0${detik}`);
+    } else if (menit < 10 && detik >= 10) {
+      // console.log(`0${menit}:${detik}`);
+      displayCountdown(`0${menit}:${detik}`);
+    } else if (menit >= 10 && detik < 10) {
+      // console.log(`${menit}:0${detik}`);
+      displayCountdown(`${menit}:0${detik}`);
+    } else if (menit >= 10 && detik >= 10) {
+      // console.log(`${menit}:${detik}`);
+      displayCountdown(`${menit}:${detik}`);
+    }
+
+    // console.log(`00:0${seconds}`);
+    seconds--;
+    if (seconds < 0) {
+      clearInterval(testvar);
+    }
+
+  }, 1000);
+
+
+  
 };
 
 module.exports = {
