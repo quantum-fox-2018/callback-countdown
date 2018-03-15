8df74604f784696cb54ad8f83d17a4efa1c40766
yasirmaulana <yasir.maulana@gmail.com>
2018-03-15 19:38:17 +0700

    week 2, day4, count down

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const { timer } = require("./controllers");
 const main = () => {
   const seconds = parseInt(process.argv[2]) || 10;
   timer(seconds);
+
 };
 
 main();
+
+
+// function display(){
+// }
+//
+// display() => {
+// }
+//
+// const main = function() = {
+// }
