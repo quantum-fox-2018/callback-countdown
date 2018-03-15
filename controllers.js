efd5ed93141ebe16acd7f4c5c1623a38cf7ed205
Zena <zena.vilenia@gmail.com>
2018-03-16 05:47:13 +0700

    tugas callback countdown zena

--- controllers.js
+++ controllers.js
@@ -1,7 +1,19 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let countdown = setInterval(() => {
+    displayCountdown(konversiMenit(seconds));
+    seconds--;
+    if(seconds < 0) {
+      clearInterval(countdown);
+    }
+  }, 1000)
+};
+
+const konversiMenit = seconds => {
+  var jam = ("0" + Math.floor(seconds/60)).slice(-2);
+  var menit = ("0" + seconds%60).slice(-2);
+  return jam + ":" + menit;
 };
 
 module.exports = {
