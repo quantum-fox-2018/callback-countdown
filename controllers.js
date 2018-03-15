c4e2142868ad19fdb09a07b4c734f9102d2260e4
Taufik Hidayat <ovick_kd@yahoo.com>
2018-03-15 19:25:11 +0700

    callback countdown

--- controllers.js
+++ controllers.js
@@ -2,7 +2,23 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
-};
+
+    var myVar = setInterval(function(){
+
+    seconds--
+
+    displayCountdown(seconds)
+
+    if (seconds < 1) {
+
+      clearInterval(myVar)
+
+    }
+
+   }, 1000);
+
+  };
+
 
 module.exports = {
   timer
--- views.js
+++ views.js
@@ -3,9 +3,37 @@ const figlet = require("figlet");
 const chalk = require("chalk");
 
 const displayCountdown = seconds => {
+
   clear();
+
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
-};
+  if (seconds < 10) {
+
+    console.log(chalk.yellow(figlet.textSync('00 : 0'+seconds)));
+
+  } else if (seconds <= 59) {
+
+    console.log(chalk.yellow(figlet.textSync('00 : '+seconds)));
+
+  } else if (seconds >= 59) {
+
+    if (Math.floor(seconds/60) < 10) {
+
+      if (Math.floor(seconds%60) < 10) {
+
+        console.log(chalk.yellow(figlet.textSync('0'+Math.floor(seconds/60) + ' : 0' +Math.floor(seconds%60))))
+
+      } else {
+
+        console.log(chalk.yellow(figlet.textSync('0'+Math.floor(seconds/60) + ' : ' +Math.floor(seconds%60))))
+
+      }
+
+    };
+
+  }
+
+}
 
 module.exports = { displayCountdown };
