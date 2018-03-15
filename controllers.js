86b0d6c83772b8eb0465f70be87fe028ee966988
barestu <fheartnet@gmail.com>
2018-03-15 17:33:27 +0700

    Callback - Countdown Timer

--- controllers.js
+++ controllers.js
@@ -2,6 +2,14 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  var interval = setInterval(function () {
+    displayCountdown(seconds);
+    seconds--;
+    if (seconds < 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
+
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -6,6 +6,18 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  var min = Math.floor(seconds/60) + '';
+  var sec = Math.floor(seconds%60) + '';
+
+  if (min.length < 2) {
+    min = '0' + min;
+  }
+
+  if (sec.length < 2) {
+    sec = '0' + sec;
+  }
+
+  console.log(chalk.white(figlet.textSync(`${min}:${sec}`)));
 };
 
 module.exports = { displayCountdown };
