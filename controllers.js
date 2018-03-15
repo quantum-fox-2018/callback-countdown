04677f79a9710a7a8e875089e1a318632a313cdd
agungcaproex <agung.caproex@gmail.com>
2018-03-15 16:46:35 +0700

    cb countdown done

--- controllers.js
+++ controllers.js
@@ -2,6 +2,30 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  setInterval(() =>{
+    
+    let min = Math.floor(seconds/60)
+    let sec = seconds - min * 60
+
+    if(min < 10){
+      min = '0' + min
+    }
+    if(sec < 10){
+      sec = '0' + sec
+    }
+    if(seconds < 0){
+      process.exit()
+    }
+
+    displayCountdown(min, sec)
+    seconds--
+    // console.log(min+':'+sec)
+  } ,1000)
+  
+
+
+
+  
 };
 
 module.exports = {
