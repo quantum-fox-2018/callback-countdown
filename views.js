ef18064832e41b9bfc26ef913ccc44ad5bb1001b
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-03-15 16:00:25 +0700

    tugas countdown rama

--- views.js
+++ views.js
@@ -2,10 +2,13 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
-const displayCountdown = seconds => {
+const displayCountdown = timeFormat => {
   clear();
+
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  console.log(chalk.red(figlet.textSync(timeFormat)));
+
 };
 
 module.exports = { displayCountdown };
