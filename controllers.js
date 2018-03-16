ad2f58cf0cbb9d75050cf67c3c5f25e201b4c387
richoandy <richoandy@gmail.com>
2018-03-16 09:07:54 +0700

    release 1

--- controllers.js
+++ controllers.js
@@ -3,12 +3,10 @@ const { displayCountdown } = require("./views");
 const timer = seconds => {
   // Your code here...
   setInterval(function(){
-    if (seconds === 0) {
-      console.log(seconds)
-      console.log("stop di 0");
+    if (seconds < 0) {
       clearInterval(this)
     }else{
-      console.log(seconds)
+      displayCountdown(seconds)
       seconds--
     }
   }, 1000)
