1bd946059405555b9655c193868c7118fd7052a0
annisaayu <annisaayu569@gmail.com>
2018-03-19 00:10:45 +0700

    countdown

--- controllers.js
+++ controllers.js
@@ -2,6 +2,15 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  function setTime(){
+    displayCountdown(seconds);
+    seconds -= 1;
+  }
+  if(seconds>0){
+    setInterval(setTime, 1000)
+  }else{
+    clearInterval(setInterval(setTime, 1000))
+  }
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -6,6 +6,35 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let menit = Math.floor(seconds/60)
+  let detik = seconds%60
+
+  if(menit >= 10 && detik >= 10){
+    figlet(`${menit} : ${detik}`,function(err, data){
+      if(!err){
+        console.log(data);
+      }
+    })
+  }else if(menit >= 10 && detik < 10){
+    figlet(`${menit} : 0${detik}`,function(err, data){
+      if(!err){
+        console.log(data);
+      }
+    })
+  }else if(menit < 10 && detik >= 10){
+    figlet(`0${menit} : ${detik}`,function(err, data){
+      if(!err){
+        console.log(data);
+      }
+    })
+  }else if(menit < 10 && detik < 10){
+    figlet(`0${menit} : 0${detik}`,function(err, data){
+      if(!err){
+        console.log(data);
+      }
+    })
+  }
+
 };
 
 module.exports = { displayCountdown };
