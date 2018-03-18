5644f524905dd02883ccf5f6f98fdc6c5c2d2a6c
David Joshua <davidjosh_87@yahoo.com>
2018-03-18 10:17:25 +0700

    callback-countdown-revisi-view.js

--- views.js
+++ views.js
@@ -4,22 +4,27 @@ const clear = require('clear');
 const figlet = require('figlet');
 const chalk = require('chalk');
 
-const displayCountdown = timeFormat => {
+const displayCountdown = seconds => {
   clear();
-  console.log(chalk.yellow(figlet.textSync('Countdown  App')));
+  console.log(chalk.green(figlet.textSync('Countdown  App')));
   // Your code here...
-  if (timeFormat == 0) {
-    console.log(chalk.white(figlet.textSync('00 : 0' + timeFormat)));
-  } else if (timeFormat < 10) {
-    console.log(chalk.red(figlet.textSync('00 : 0' + timeFormat)));
-  } else if (timeFormat < 60) {
-    console.log(chalk.cyan(figlet.textSync('00 : ' + timeFormat)));
-  } else if(timeFormat == 60){
-    console.log(chalk.yellow(figlet.textSync('01 : 0' + timeFormat%60)));
-  } else if(timeFormat > 60){
-    console.log(chalk.yellow(figlet.textSync('01 : 0' + timeFormat%60)));
-  }
 
+  let min = Math.floor(seconds / 60);
+  let sec = Math.floor(seconds % 60);
+
+  if (seconds == 0) {
+    console.log(chalk.white(figlet.textSync(`0${min}:0${sec}`)));
+  } else if (seconds < 10) {
+    console.log(chalk.red(figlet.textSync(`0${min}:0${sec}`)));
+  } else if (seconds < 60) {
+      console.log(chalk.yellow(figlet.textSync(`0${min}:${sec}`)));
+  } else if (seconds >= 60) {
+    if (sec < 10) {
+      console.log(chalk.cyan(figlet.textSync(`0${min}:0${sec}`)));
+    } else if (min < 10) {
+      console.log(chalk.cyan(figlet.textSync(`0${min}:${sec}`)));
+    }
+  }
 };
 
 module.exports = {
